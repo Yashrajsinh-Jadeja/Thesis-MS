2548a8ccdbe86008ea44fefa26f1b8cf9b02f685
Yashrajsinh Jadeja <52894785+Yashrajsinh-Jadeja@users.noreply.github.com>
2020-01-08 19:57:47 +0530

    Update flanking_updated.py
    literally shaking rn

--- flanking_updated.py
+++ flanking_updated.py
@@ -1,33 +1,51 @@
-#ID
-#What this script does : Useful when trying to obtain flanking regions to create miRNA precursors from BLAST hits across a genome (miRNA hits with genome as reference).
+##ID
+##What this script does : Useful when trying to obtain flanking regions to create miRNA precursors from BLAST hits across a genome (miRNA hits with genome as reference).
 
-#What you need : Your BLAST results along with the appropriate (start and end) query + subject coordinates of every match.
-#Your reference genome in a fasta format.
-#A file containing the accessions of the matches in a tabular form. (A file containing accessions, seperated by a new-line).
+##What you need : Your BLAST results along with the appropriate (start and end) query + subject coordinates of every match.
+##Your reference genome in a fasta format.
+##A file containing the accessions of the matches in a tabular form. (A file containing accessions, seperated by a new-line).
 #You can rename the filenames in the script.
 
-#Flanking sequence from the BLAST hits across the genome.
-#Yashrajsinh Jadeja
+##Flanking sequence from the BLAST hits across the genome.
+##Yashrajsinh Jadeja 8th Jan 2020
 #TODO: Fine-tune according to the pipeline.
 
 from Bio import SeqIO
 from Bio.Seq import Seq
-record_index = SeqIO.index("sequence.fasta", "fasta") #Indexing the reference genome.
+record_index = SeqIO.index("sequence.fasta", "fasta") ##Indexing the reference genome.
 
-accessions = []
+accessions = [] ##Declaring lists.
+start = []
+end = []
+ctr = 0
 
-for x in open("file"):
-    accessions.append(x.replace("\n","")) #Fetching accessions from the file.
+for x in open("gene.txt"):
+    accessions.append(x.replace("\n","")) ##Fetching accessions from the file.
 
-#file=open("precursors.fasta","w") #TODO writing a fasta.
+for x in open("start.txt"):
+    start.append(int(x.replace("\n",""))) ##Fetching start coordinates from the file.
 
-flank = int(input("Enter the flank length.")) #Input flank length for precursors.
+for x in open("end.txt"):
+    end.append(int(x.replace("\n",""))) ##Fetching end coordinates from the file.
+
+flank = int(input("Enter the flank length.")) ##Input flank length for precursors.
 
 for x in accessions:
-    start=int(input(f"\nEnter the S T A R T position of the hit for sequence acc {x}\n")) #Enter the start coordinate.
-    end=int(input(f"\nEnter the E N D position of the hit for sequence acc {x}\n")) #Enter the end coordinate.
-    print(f"\nMotif: {record_index[x].seq[start:end]}")
-    print(f"\nLeft Flank: {record_index[x].seq[start-flank:start]}")
-    print(f"\nRight Flank: {record_index[x].seq[end:end+flank]}")
-
-#file.close()
+    # start=int(input(f"\nEnter the S T A R T position of the hit for sequence acc {x}\n")) #Enter the start coordinate.
+    # end=int(input(f"\nEnter the E N D position of the hit for sequence acc {x}\n")) #Enter the end coordinate.
+    print(f"\nMotif: {record_index[x].seq[start[ctr]:end[ctr]]}")
+    print(f"\nLeft Flank: {record_index[x].seq[start[ctr]-flank:start[ctr]]}")
+    print(f"\nRight Flank: {record_index[x].seq[end[ctr]:end[ctr]+flank]}")
+    file=open(f"pni_yj_precursor_recordnum_{ctr+1}.fasta","w")
+    file.write(f">pni_yj_precursor_recordnum_{ctr+1}_coord_{start[ctr]}:{end[ctr]}\n")
+    file.write(str(record_index[x].seq[start[ctr]-flank:start[ctr]] + record_index[x].seq[start[ctr]:end[ctr]] + record_index[x].seq[end[ctr]:end[ctr]+flank]))
+    file.close()
+    ctr += 1
+print("\n")
+print("*" * 13)
+print("All done!")
+print("*" * 13)
+print("\n")
+print(f"The number of precursors generated are {ctr}.")
+print(f"The last of precursor's gene ID was {x} and it's number was {ctr}.")
+print(f"The last start coordinate was : {start[ctr-1]} and end coordinate was {end[ctr-1]}")

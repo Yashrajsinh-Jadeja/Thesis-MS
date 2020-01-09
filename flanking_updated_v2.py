2ef1e5cb6c104212bd15fa4f7d72a52dd6f3f21f
Yashrajsinh Jadeja <52894785+Yashrajsinh-Jadeja@users.noreply.github.com>
2020-01-09 20:26:24 +0530

    Update and rename flanking_updated.py to flanking_updated_v2.py
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- flanking_updated_v2.py
+++ flanking_updated_v2.py
@@ -15,10 +15,8 @@
 ##Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license.
 ##Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
 
-from Bio import SeqIO
-from Bio.Seq import Seq
-record_index = SeqIO.index("sequence.fasta", "fasta") ##Indexing the reference genome.
-
+from pyfaidx import Fasta
+record_index = Fasta("sequence.fasta")
 accessions = [] ##Declaring lists.
 start = []
 end = []
@@ -33,24 +31,44 @@ for x in open("start.txt"):
 for x in open("end.txt"):
     end.append(int(x.replace("\n",""))) ##Fetching end coordinates from the file.
 
-flank = int(input("Enter the flank length.")) ##Input flank length for precursors.
+flank = int(input("Enter the flank length. : ")) ##Input flank length for precursors.
+
+mot_strt = open("motif_start","w")
+mot_end = open("motif_end","w")
 
 for x in accessions:
-    # start=int(input(f"\nEnter the S T A R T position of the hit for sequence acc {x}\n")) #Enter the start coordinate.
-    # end=int(input(f"\nEnter the E N D position of the hit for sequence acc {x}\n")) #Enter the end coordinate.
-    print(f"\nMotif: {record_index[x].seq[start[ctr]:end[ctr]]}")
-    print(f"\nLeft Flank: {record_index[x].seq[start[ctr]-flank:start[ctr]]}")
-    print(f"\nRight Flank: {record_index[x].seq[end[ctr]:end[ctr]+flank]}")
+    motif = str(record_index[x][start[ctr]-1:end[ctr]].seq)
+    lflank = str(record_index[x][start[ctr]-flank-1:start[ctr]-1].seq)
+    rflank = str(record_index[x][end[ctr]:end[ctr]+flank].seq)
+    seq = lflank + motif + rflank
+    ##motif_start = seq.find(motif) This will apply to other cases where we want to locate exact motif in the seq.
+    ##MOTIF start will be flank here as we know the flank length.
+    mot_strt.write(f"{str(flank)}\n")
+    mot_end.write(f"{str(flank+len(motif))}\n")
+    #motif_end = motif_start + len(motif)
     file=open(f"pni_yj_precursor_recordnum_{ctr+1}.fasta","w")
     file.write(f">pni_yj_precursor_recordnum_{ctr+1}_coord_{start[ctr]}:{end[ctr]}\n")
-    file.write(str(record_index[x].seq[start[ctr]-flank:start[ctr]] + record_index[x].seq[start[ctr]:end[ctr]] + record_index[x].seq[end[ctr]:end[ctr]+flank]))
+    file.write(seq)
     file.close()
+    print(f"\nGENE ACC: {x}")
+    print(f"ITERATION NUMBER: {ctr+1}")
+    print(f"\nMotif: {motif}")
+    print(f"Left Flank: {lflank}")
+    print(f"Right FLank: {rflank}")
+    print(f"The motif coordinate is: {flank} - {flank+len(motif)}\n")
+    print(f"The length of motif is: {len(motif)}")
+    #motif = len(str(record_index[x].seq[start[ctr]:end[ctr]]))
+    # print(f"The motif coordinate is !!TEST!!{lflank_len} : {tot_len - rflank_len}")
     ctr += 1
-print("\n")
+
+mot_strt.close()
+mot_end.close()
+
+print("\n\n")
 print("*" * 13)
 print("All done!")
 print("*" * 13)
-print("\n")
+print("\n\n")
 print(f"The total number of precursors generated are {ctr}.")
 print(f"The last precursor's gene ID was {x} and it's number was {ctr}.")
-print(f"The last start coordinate was : {start[ctr-1]} and end coordinate was {end[ctr-1]}")
+print(f"The last start coordinate was: {start[ctr-1]} and end coordinate was: {end[ctr-1]}")

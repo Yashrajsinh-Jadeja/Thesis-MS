cb969fd5d93aaa15a005fc138e70acf73cbb11ce
Yashrajsinh Jadeja <52894785+Yashrajsinh-Jadeja@users.noreply.github.com>
2020-01-16 20:24:32 +0530

    Update remover.sh
    Added documentation.

--- remover.sh
+++ remover.sh
@@ -1,5 +1,5 @@
-#This shell-script had to be written as the "shopt -s extglob" does not work in the python script.
-#NOTE: ALWAYS RUN THIS IN AN EXPERIMENT FOLDER AS THIS SCRIPT IS DESIGNED TO REMOVE EVERYTHING THAT ISN'T THE BELOW MENTIONED FILES.
+#This shell-script had to be written as the "shopt -s extglob" does not work in the python script. This is just a workaround.
+#NOTE: ALWAYS RUN THIS IN AN EXPERIMENTAL FOLDER WITHOUT CRUCIAL DATA AS THIS SCRIPT IS DESIGNED TO REMOVE EVERYTHING THAT ISN'T THE BELOW MENTIONED FILES/FILE EXTENSIONS.
 shopt -s extglob
 rm !(*.py|*.fasta|motif*|*.ct|*.EPS|*.jar|*.sh|*.png)
 echo "***Removed redundant files***"
